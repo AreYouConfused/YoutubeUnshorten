a85bb6e15f04017ec74c02cfaf95096f3db7842f
John <git@stillconfusing.net>
2022-04-15 13:20:58 -0400

    now intercepts http request to redirect

--- unshorten.js
+++
@@ -1,8 +0,0 @@
-let curStatus = browser.storage.local.get("shortsEnabled")
-
-curStatus.then((object) => {
-	if (object["shortsEnabled"] === false) {
-		window.location.href = "https://www.youtube.com/watch?v=" + window.location.pathname.split("/")[2]
-	}
-})
-
